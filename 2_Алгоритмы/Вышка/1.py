87eb2853298ce7adc76e444d6bf6849370dbf8c0
deusmouse <smulsky.rv@gmail.com>
2022-08-01 10:23:46 +0300

    ++ вышка

---
+++ 2_Алгоритмы/Вышка/1.py
@@ -0,0 +1,22 @@
+'''
+Напишите алгоритм поиска двух элементов в массиве натуральных чисел, произведение которых
+максимально. Определите вычислительную сложность вашего алгоритма.
+'''
+
+data = [1, 3, 5, 7, 2, 1, 5, 9, 6, 1]
+data = []
+data = [1, 2, 5, 7, 2, 1, 5, -9, 6, 1]
+
+best = 0
+for index in range(len(data)):
+    if not data:
+        break
+    elif not isinstance(data[index], int):
+        break
+    elif index == 0:
+        continue
+    current = data[index-1] * data[index]
+    if current > best:
+        best = current
+
+print(best)
