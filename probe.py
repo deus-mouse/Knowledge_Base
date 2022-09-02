4cd74e628f9befb1437b62c4ca351b9bbe15b8d2
deusmouse <smulsky.rv@gmail.com>
2022-09-02 10:58:48 +0300

    3

--- probe.py
+++ probe.py
@@ -15,3 +15,12 @@ exec("%s = %d" % (str, 5000))
 
 #print string
 print("output : ", Pythonpool)
+
+#input String
+str = "Pythonpool"
+
+#apply exec() method
+exec("%s = %d" % (str, 5000))
+
+#print string
+print("output : ", Pythonpool)
