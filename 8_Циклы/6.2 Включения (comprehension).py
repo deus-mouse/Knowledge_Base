932431f4ef84e293a75a56cb0509d090519a302b
rsmulskii <rsmulskii@neuro.net>
2023-01-17 12:49:54 +0100

    + 6.2 + 6.3 Включения (comprehension)

---
+++ 8_Циклы/6.2 Включения (comprehension).py
@@ -0,0 +1,22 @@
+squares = [x * x for x in range(10)]
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+
+my_items = ['a', 'b', 'c']
+f = [item * 3 for item in my_items]
+# ['aaa', 'bbb', 'ccc']
+
+
+# С фильтрацией
+even_squares = [x * x for x in range(10) if x % 2 == 0]
+# [0, 4, 16, 36, 64]
+
+
+# включение в множество
+my_set = {x * x for x in range(-9, 10)}
+# {64, 1, 0, 36, 4, 9, 16, 81, 49, 25}  неупорядочено
+
+
+# включение в словарь:
+my_dict = {x: x * x for x in range(5)}
+# {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
--- probe.py
+++ probe.py
@@ -1,14 +1,5 @@
 my_items = ['a', 'b', 'c']
 
-i = 0
-while i < len(my_items):
-    print(my_items[i])
-    i += 1
+f = [item * 3 for item in my_items]
 
-for i in my_items:
-    print(i)
-
-
-print(f'{enumerate(my_items)=}')
-for i, item in enumerate(my_items):
-    print(f'{i}: {item}')
+print(f)
