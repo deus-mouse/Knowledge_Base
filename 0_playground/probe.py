fbd5867d55846abf3fa04112c74811bbe3030c2d
deusmouse <smulsky.rv@gmail.com>
2023-07-11 10:42:45 +0200

    алгоритм поиска подстрока в строки с префикс-функцией KMP.py added

--- 0_playground/probe.py
+++ 0_playground/probe.py
@@ -1,9 +1,81 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
 
 
-f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(f'{len(f)=}')
+class AbstractFactory(ABC):
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
 
-for i in range(len(f)):
-    print(f'{i=}')
-    print(f'{f[i]=}')
-    print('---------------')
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
+        pass
+
+
+class Factory1(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ProductA1()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ProductB1()
+
+
+class Factory2(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ProductA2()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ProductB2()
+
+
+class AbstractProductA(ABC):
+    @abstractmethod
+    def run_a(self):
+        pass
+
+
+class ProductA1(AbstractProductA):
+    def run_a(self):
+        print(f'run ProductA1')
+
+
+class ProductA2(AbstractProductA):
+    def run_a(self):
+        print(f'run ProductA2')
+
+
+class AbstractProductB(ABC):
+    @abstractmethod
+    def run_b(self):
+        pass
+
+
+class ProductB1(AbstractProductB):
+    def run_b(self):
+        print(f'RUN ProductB1')
+
+
+class ProductB2(AbstractProductB):
+    def run_b(self):
+        print(f'RUN ProductB2')
+
+
+def client_code(factory: AbstractFactory) -> None:
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    product_a.run_a()
+    product_b.run_b()
+
+
+if __name__ == "__main__":
+    """
+    Клиентский код может работать с любым конкретным классом фабрики.
+    """
+    print("Client: Testing client code with the first factory type:")
+    client_code(Factory1())
+
+    print("\n")
+
+    print("Client: Testing the same client code with the second factory type:")
+    client_code(Factory2())
--- 0_playground/probe2.py
+++ 0_playground/probe2.py
@@ -1,3 +1,66 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
 
 
-print(list(list([1, 2])))
+
+class AbstractFactory(ABC):
+    @abstractmethod
+    def create_product_a(self) -> AbstractProduct:
+        pass
+
+    @abstractmethod
+    def create_product_b(self) -> AbstractProduct:
+        pass
+
+
+class Factory1(AbstractFactory):
+    def create_product_a(self) -> AbstractProduct:
+        return ProductA1()
+
+    def create_product_b(self) -> AbstractProduct:
+        return ProductB1()
+
+
+class Factory2(AbstractFactory):
+    def create_product_a(self) -> AbstractProduct:
+        return ProductA2()
+
+    def create_product_b(self) -> AbstractProduct:
+        return ProductB2()
+
+
+class AbstractProduct(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class ProductA1(AbstractProduct):
+    def run(self):
+        print('ProductA1')
+
+
+class ProductA2(AbstractProduct):
+    def run(self):
+        print('ProductA2')
+
+
+class ProductB1(AbstractProduct):
+    def run(self):
+        print('ProductB1')
+
+
+class ProductB2(AbstractProduct):
+    def run(self):
+        print('ProductB2')
+
+
+def client_code(factory: AbstractFactory):
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    product_a.run()
+    product_b.run()
+
+client_code(Factory1())
+client_code(Factory2())
