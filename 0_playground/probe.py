aca91b92a47151adf2d57b4b81477db617ba3bb5
rsmulskii <rsmulskii@neuro.net>
2023-07-16 13:47:24 +0200

    ++ prototype

--- 0_playground/probe.py
+++ 0_playground/probe.py
@@ -1,31 +1,101 @@
-
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        l1_str = str(l1)
-        l2_str = str(l2)
-        l1_str = ''.join(str(x) for x in l1_str[::-1] if x.isdigit())
-        l2_str = ''.join(str(x) for x in l2_str[::-1] if x.isdigit())
-        result_int = int(l1_str) + int(l2_str)
-        result_str = str(result_int)
-        result_list = [int(x) for x in result_str]
-        return result_list[::-1]
+import copy
 
 
-l1 = [2,4,3]
-l2 = [5,6,4]
+class SelfReferencingEntity:
+    def __init__(self):
+        self.parent = None
 
-solution = Solution()
-print(f'{solution.addTwoNumbers(l1, l2)=}')
+    def set_parent(self, parent):
+        self.parent = parent
 
-# print(str(l1))
+
+class SomeComponent:
+    def __init__(self, some_int, some_list_of_objects, some_circular_ref):
+        self.some_int = some_int
+        self.some_list_of_objects = some_list_of_objects
+        self.some_circular_ref = some_circular_ref
+
+    def __copy__(self):
+        some_list_of_objects = copy.copy(self.some_list_of_objects)
+        some_circular_ref = copy.copy(self.some_circular_ref)
+
+        print(f'{self.__class__=}')
+        new = self.__class__(self.some_int, some_list_of_objects, some_circular_ref)
+        print(f'{self.__dict__=}')
+        print(f'{new.__dict__=}')
+        new.__dict__.update(self.__dict__)
+
+        return new
+
+    def __deepcopy__(self, memo={}):
+        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
+        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
+
+        new = self.__class__(self.some_int, some_list_of_objects, some_circular_ref)
+        new.__dict__ = copy.deepcopy(self.__dict__, memo)
+
+        return new
+
+
+if __name__ == '__main__':
+    list_of_objects = [1, {1, 2, 3}, [1, 2, 3]]
+    circular_ref = SelfReferencingEntity()
+    component = SomeComponent(23, list_of_objects, circular_ref)
+    circular_ref.set_parent(component)
+
+    shallow_copied_component = copy.copy(component)
+
+    shallow_copied_component.some_list_of_objects.append("another object")
+    if component.some_list_of_objects[-1] == "another object":
+        print(
+            "another object YES"
+        )
+    else:
+        print(
+            "another object NO"
+        )
+
+    component.some_list_of_objects[1].add(4)
+    if 4 in shallow_copied_component.some_list_of_objects[1]:
+        print(
+            "4 YES"
+        )
+    else:
+        print(
+            "4 NO"
+        )
+
+    deep_copied_component = copy.deepcopy(component)
+    deep_copied_component.some_list_of_objects.append("one more object")
+    if component.some_list_of_objects[-1] == "one more object":
+        print(
+            "one more object YES"
+        )
+    else:
+        print(
+            "one more object NO"
+        )
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(10)
+    if 10 in deep_copied_component.some_list_of_objects[1]:
+        print(
+            "10 YES"
+        )
+    else:
+        print(
+            "10 NO"
+        )
+
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent)}"
+    )
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
+    )
+    print(
+        "^^ This shows that deepcopied objects contain same reference, they "
+        "are not cloned repeatedly."
+    )
