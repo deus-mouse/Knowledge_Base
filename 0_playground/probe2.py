b985a443524050a29dbb07390e1479c1e62a6c58
rsmuslkii <rsmuslkii@voctiv.com>
2023-09-09 18:48:10 +0200

    ++ Наблюдатель_Observer_GOF.py, граф

--- 0_playground/probe2.py
+++ 0_playground/probe2.py
@@ -1,96 +1,18 @@
-import inspect
-from threading import Thread
-from time import sleep
+n = 6
+adjList = [[] for _ in range(n)]
 
-nlu_results = []
+edges = [(0, 1), (0, 4), (0, 5), (4, 5), (1, 4), (1, 3), (2, 3), (2, 4)]
 
-
-def logger(data):
-    '''usage: logger(f'{MSISDN=}')'''
-    func = inspect.stack()[1][3]
-    if func == '<module>':
-        func = ''
-    print(f'+++ {func}: {data}')
+for (src, dest) in edges:
+    print(f'{src=}, {dest=}')
+    adjList[src].append(dest)
+    adjList[dest].append(src)
+    print(f'{adjList=}')
 
 
-class CustomThread(Thread):
-    # constructor
-    def __init__(self, r):
-        # execute the base constructor
-        Thread.__init__(self)
-        # set a default value
-        self.value = None
-        self.nlu_results = []
-        self.r = r
-
-    def run(self):
-        if self.r:
-            payload = {
-                'input': self.r,
-                'regex': self.r,
-                'nlu': {},
-            }
-
-            logger('nlu response:')
-            r_nlu = None
-            try:
-                # 1st option how we can realize reserve nlu recognition
-                r_nlu = 'r_nlu'  # <- nlu
-
-                # 2nd option
-                # r.set_nlu_ver('3.0')
-                # r_nlu = nlu.extract(r.utterance())
-
-                if r_nlu:
-                    intents = ['r_nlu._intents']
-
-                    # removing patterns from r_nlu result
-                    if intents:
-                        payload.update({'nlu': intents})
-                sleep(5)
-
-            except Exception as e:
-                logger('reserve_recognition, error message: ', e)
-
-            self.nlu_results.append(payload)
-            # return func(r if (r.has_intents() or r.has_entities() or not r_nlu) else r_nlu)
-            self.value = r_nlu
-            # return r_nlu
+for u in range(n):
+    print(f'{u=}')
 
 
-def nlu(func):
-    '''decorator for additional recognition by regex + nlu'''
-
-    def surrogate(r):  # r <- regex
-        nlu_thread = CustomThread(r)
-        nlu_thread.start()
-        if r and r == 'R':
-            print(f'{nlu_thread.nlu_results=}')
-            return func(r)
-        else:
-            nlu_thread.join()
-            print(f'{nlu_thread.nlu_results=}')
-            return func(nlu_thread.value)
-    return surrogate
-
-
-
-@nlu
-def logic(r):
-    logger(f'{r=}')
-
-
-logic(r='R')
-print('========================')
-logic(r='NLU')
-
-
-# # create a new thread
-# thread = CustomThread()
-# # start the thread
-# thread.start()
-# # wait for the thread to finish
-# thread.join()
-# # get the value returned from the thread
-# data = thread.value
-# print(data)
+s = set([1, 1, 2])
+print(f'{s=}')
--- 0_playground/probe3.py
+++ 0_playground/probe3.py
@@ -1,32 +1,11 @@
-# SuperFastPython.com
-# example of returning a value from a thread
-from time import sleep
-from threading import Thread
+result = {0: 1, 1: 2, 2: 1, 3: 3, 4: 3, 5: 2}
+colors = max(result.values())
+print(colors)
 
+new_result = {i: [] for i in range(1, colors+1)}
+print(f'{new_result=}')
 
-# custom thread
-class CustomThread(Thread):
-    # constructor
-    def __init__(self):
-        # execute the base constructor
-        Thread.__init__(self)
-        # set a default value
-        self.value = None
+for k, v in result.items():
+    new_result[v].append(k)
 
-    # function executed in a new thread
-    def run(self):
-        # block for a moment
-        sleep(1)
-        # store data in an instance variable
-        self.value = 'Hello from a new thread'
-
-
-# create a new thread
-thread = CustomThread()
-# start the thread
-thread.start()
-# wait for the thread to finish
-thread.join()
-# get the value returned from the thread
-data = thread.value
-print(data)
+print(f'{new_result=}')
